29b7c29f76a3a0661450c0e024057abdc0b305b9
glenio-juste <glenio.juste@gmail.com>
2021-06-20 21:11:44 -0300

    Commit do projeto

---
+++ front-end/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/glenio-juste" target="_blank" rel="noreferrer">Glenio Juste</a></p>
+                <p className="text-light"><small>
+                    Evento promovido pela escola DevSuperior</small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
