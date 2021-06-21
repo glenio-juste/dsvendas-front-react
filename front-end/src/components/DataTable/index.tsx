86c398472adc42524520b576187b6c9579961ca6
glenio-juste <glenio.juste@gmail.com>
2021-06-20 21:33:36 -0300

    Refatoração de código

--- front-end/src/components/DataTable/index.tsx
+++ front-end/src/components/DataTable/index.tsx
@@ -14,74 +14,74 @@ const DataTable = () => {
                 </thead>
                 <tbody>
                     <tr>
+                        <td>20/04/2021</td>
+                        <td>Aeronauta Barata</td>
+                        <td>34</td>
+                        <td>22</td>
+                        <td>1527.00</td>
+                    </tr>
+                    <tr>
+                        <td>20/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>73</td>
+                        <td>25</td>
+                        <td>1817.00</td>
+                    </tr>
+                    <tr>
+                        <td>21/04/2021</td>
+                        <td>Asteróide Silverio</td>
+                        <td>55</td>
+                        <td>20</td>
+                        <td>1547.00</td>
+                    </tr>
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
+                        <td>100</td>
+                        <td>77</td>
+                        <td>1000.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Disney Chaplin Milhomem da Silva</td>
+                        <td>90</td>
+                        <td>45</td>
+                        <td>4523.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Dezêncio Feverêncio de Oitenta e Cinco</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>23/04/2021</td>
+                        <td>Mimaré Índio Brazileiro de Campos</td>
+                        <td>200</td>
+                        <td>99</td>
+                        <td>99925.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>23/04/2021</td>
+                        <td>Napoleão Sem Medo e Sem Mácula</td>
+                        <td>300</td>
+                        <td>250</td>
+                        <td>55002.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>25/04/2021</td>
+                        <td>Oceâno Atlântico Linhares</td>
+                        <td>90</td>
+                        <td>60</td>
+                        <td>8025.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>26/04/2021</td>
+                        <td>Plácido e Seus Companheiros</td>
+                        <td>745</td>
+                        <td>150</td>
+                        <td>300050.00</td>
                     </tr>
                 </tbody>
             </table>
